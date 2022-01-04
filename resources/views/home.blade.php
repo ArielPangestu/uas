6204269833f1cc2fdcb17361bc26e9ed8a58752c
Ariel Pangestu <arielpangestu141@gmail.com>
2022-01-04 11:57:47 +0700

    Index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,25 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <img src="{{ url('image/Jam.png') }}" class="rounded mx-auto d-block" width="500" alt="">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="image/Jam.png" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="image/Jam 2.png" alt="Second slide" width="612" height="408">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+          </div>
             <br>
             <div class="card">
                 <div class="card-header">Dashboard</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                  <img src="{{ url('image/Logo.png') }}" width="80">
-                    {{ config('app.name', 'Laravel') }}
+                 <strong>Trouble Watch Store</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>

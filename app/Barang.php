eaed09ea3be3ac147aa56745e94d9c6ba4399863
Ariel Pangestu <arielpangestu141@gmail.com>
2022-01-04 12:44:54 +0700

    model

--- app/Barang.php
+++ app/Barang.php
@@ -6,8 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    public function pesanan_detail() 
-	{
-	     return $this->hasMany('App\PesananDetail','barang_id', 'id');
-	}
+    //
 }
--- app/PesananDetail.php
+++ app/PesananDetail.php
@@ -6,13 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class PesananDetail extends Model
 {
-    public function barang()
-	{
-	      return $this->belongsTo('App\Barang','barang_id', 'id');
-	}
-
-	public function pesanan()
-	{
-	      return $this->belongsTo('App\Pesanan','pesanan_id', 'id');
-	}
+   //
 }
